8630071167133ea32bb6a642a66f7750cf348e5a
vasilijus <vasiliokas@gmail.com>
2019-10-26 13:37:59 +0100

    init

---
+++ src/HiLo.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class HiLo {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		// TODO Generate a rand number 
+		int theNumber = (int) (Math.random() * 100 + 1);
+		System.out.println(theNumber);
+		int guess = 0;
+		System.out.println("Guess a number between 1 and 100: ");
+		guess = scan.nextInt();
+		
+	}
+
+}
