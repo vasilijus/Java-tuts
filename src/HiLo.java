eaeb78c4b752e90fdb3bf1251ef3e400b79a005a
vasilijus <vasiliokas@gmail.com>
2019-10-26 14:15:44 +0100

    added play again func

--- src/HiLo.java
+++ src/HiLo.java
@@ -4,23 +4,29 @@ public class HiLo {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		
-		// TODO Generate a rand number 
-		int theNumber = (int) (Math.random() * 100 + 1);
-		// System.out.println(theNumber);
-		int guess = 0;
-		while (guess != theNumber) {
-			System.out.println(" Guess a number between 1 and 100: ");
-			guess = scan.nextInt();
-//			System.out.println("You entered " + guess + ".");
-			if ( guess < theNumber ) {
-				System.out.println(guess + " is too low. Try again. ");
-			} else if ( guess > theNumber ) {
-				System.out.println(guess + " is too high. Try again. ");
-			} else {
-				System.out.println("Congrats, You guessed right: " + guess);
-			}
-		}
+		String playAgain = "";		
+
+		do {
+			// TODO Generate a rand number 
+			int theNumber = (int) (Math.random() * 100 + 1);
+			// System.out.println(theNumber);
+			int guess = 0;
+			
+			while (guess != theNumber) {
+				System.out.println(" Guess a number between 1 and 100: ");
+				guess = scan.nextInt();
+
+				if ( guess < theNumber ) {
+					System.out.println(guess + " is too low. Try again. ");
+				} else if ( guess > theNumber ) {
+					System.out.println(guess + " is too high. Try again. ");
+				} else {
+					System.out.println("Congrats, You guessed right: " + guess);
+				}
+			} // End of while
+			System.out.println("Would you like to play again ? ");
+			playAgain = scan.next();
+		} while ( playAgain.equalsIgnoreCase("y")); // End of Do 
 		
 	}
 
