5a30168d9de272640253d7063447f56df2f5f954
vasilijus <vasiliokas@gmail.com>
2019-10-26 14:04:19 +0100

    finish game , low spec

--- src/HiLo.java
+++ src/HiLo.java
@@ -7,10 +7,20 @@ public class HiLo {
 		
 		// TODO Generate a rand number 
 		int theNumber = (int) (Math.random() * 100 + 1);
-		System.out.println(theNumber);
+		// System.out.println(theNumber);
 		int guess = 0;
-		System.out.println("Guess a number between 1 and 100: ");
-		guess = scan.nextInt();
+		while (guess != theNumber) {
+			System.out.println(" Guess a number between 1 and 100: ");
+			guess = scan.nextInt();
+//			System.out.println("You entered " + guess + ".");
+			if ( guess < theNumber ) {
+				System.out.println(guess + " is too low. Try again. ");
+			} else if ( guess > theNumber ) {
+				System.out.println(guess + " is too high. Try again. ");
+			} else {
+				System.out.println("Congrats, You guessed right: " + guess);
+			}
+		}
 		
 	}
 
